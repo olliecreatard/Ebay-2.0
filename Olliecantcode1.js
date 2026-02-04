dc6ca3e5d717e8704eab03a0830e0afbfffea297
olliecreatard <29oliver.creagan@boiseschools.net>
2026-02-03 17:39:17 -0700

    Update Olliecantcode1.js

--- Olliecantcode1.js
+++ Olliecantcode1.js
@@ -1,16 +1,1 @@
 
-var btn = document.querySelector('.button');
-
-
-btn.onclick = function(event) {
- 
-  event.preventDefault();
-
-
-  var username = prompt("Please enter your username:");
-
- 
-  if (username) {
-    alert("You entered: " + username);
-  }
-};
